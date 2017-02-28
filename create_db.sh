d10efb937332b17d531bc80d921dd947bc311ec2
Matt Le <ml9951@rit.edu>
2017-02-28 11:52:04 -0500

    Adding script to create database

---
+++ create_db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $(psql -lqt | cut -d \| -f 1 | grep sociome) ]; then
+	echo "Dropping database..."
+	dropdb sociome
+fi
+
+echo "Creating database..."
+createdb sociome
+
+cat master.sql | psql sociome
+
+rm master.sql
